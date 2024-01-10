488065c9770787e261dcaf94733a9e6d406f837d
TheLurch27 <kevinnituom1996.sae@t-online.de>
2024-01-10 14:37:54 +0100

    Added Map and Player + PlayerController

--- Escape_Room_SAE/Escape_Room_SAE/Game.cs
+++ Escape_Room_SAE/Escape_Room_SAE/Game.cs
@@ -11,18 +11,39 @@ namespace Escape_Room_SAE
         private enum EMapTiles
         {
             free = -1,
-            wall = 0,
+            wall
         }
-        private const int MAP_SIZE = 10;
-        private static int[,] map;
 
+        private static string[] mapTileChar = new string[]
+        {
+            "  ",
+            "██"
+        };
+
+        private const int MAP_SIZE = 9;
+
+        private static int[,] map;
         private static int playerX;
         private static int playerY;
+        private const string playerCharacter = ":)";
 
         public void Run() // Neue "Main"
         {
             WelcomeMessage();
+            UserInputMapSize();
             DrawMap();
+            PrintMap();
+
+            // __________________________________________________________
+            // Trouple - Einfach NEIN!
+
+            // (int Left, int Top) cursorPos = Console.GetCursorPosition();
+            // Console.SetCursorPosition(playerX, playerY);
+            // Console.Write(playerCharacter);
+            // Console.SetCursorPosition(cursorPos.Left, cursorPos.Top);
+
+            // __________________________________________________________
+
             DrawPlayer();
 
             Console.ReadLine();
@@ -36,7 +57,13 @@ namespace Escape_Room_SAE
             Console.Clear();
         }
 
+        public void UserInputMapSize()
+        {
+
+        }
+
         public void DrawMap()
+            // Komplette Initialisierung der Map
         {
             map = new int[MAP_SIZE, MAP_SIZE];
             for (int y = 0; y < MAP_SIZE; y++)
@@ -53,9 +80,58 @@ namespace Escape_Room_SAE
                     }
                 }
             }
-
             playerX = MAP_SIZE / 2;
             playerY = MAP_SIZE / 2;
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Clear();
+                PrintMap();
+                ConsoleKeyInfo keyInput = Console.ReadKey();
+                switch (keyInput.Key)
+                {
+                    case ConsoleKey.Escape:
+                        isRunning = false;
+                        break;
+                    case ConsoleKey.W:
+                        break;
+                    case ConsoleKey.A:
+                        break;
+                    case ConsoleKey.S:
+                        break;
+                    case ConsoleKey.D:
+                        break;
+                    case ConsoleKey.UpArrow:
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        break;
+                    case ConsoleKey.DownArrow:
+                        break;
+                    case ConsoleKey.RightArrow:
+                        break;
+                }
+        }
+
+        private static void PrintMap()
+        {
+            for (int y = 0; y < MAP_SIZE; y++)
+            {
+                for(int x = 0; x < MAP_SIZE; x++)
+                {
+                    if(y == playerY && x == playerX)
+                    {
+                        Console.Write(playerCharacter + "");
+                    }
+                    else
+                    {
+                        int mapValue = map[x, y];
+                        Console.Write(mapTileChar[mapValue + 1]);
+                    }
+                }
+
+                Console.WriteLine();
+            }
         }
 
         public void DrawPlayer()
