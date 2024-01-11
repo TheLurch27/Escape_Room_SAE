1415c58f2750ad1b6ab1d258f9d8bac652bd070c
TheLurch27 <kevinnituom1996.sae@t-online.de>
2024-01-11 21:09:45 +0100

    Key (+ functions), character (+ functions) and door added.

--- Escape_Room_SAE/Escape_Room_SAE/Game.cs
+++ Escape_Room_SAE/Escape_Room_SAE/Game.cs
@@ -8,135 +8,332 @@ namespace Escape_Room_SAE
 {
     internal class Game
     {
+        // Variablen für die Map
+        private static int MAP_SIZE;
+        private static int[,] map;
+
+        // Variablen für die Eingabe und Handlung des Spielers
+        private int userInput;
+        private bool isValidInput = false;
+        private bool isRunning = true;
+        private bool isKeyCollected = false;
+
+        // Variablen für den Character
+        private static int playerX;
+        private static int playerY;
+        private static string playerChar = ":)";
+
+        // Mehr Variablen bzw ein ENUM für die Map
         private enum EMapTiles
         {
-            free = -1,
-            wall
+            floor = -1,
+            wall,
+            key,
+            openDoor,
+            closedDoor
         }
 
+        // Noch mehr Variablen für die Map. Mensch das hört nicht auf....
         private static string[] mapTileChar = new string[]
         {
             "  ",
-            "██"
+            "██",
+            "O╣",
+            "▒▒",
+            "▓▓"
         };
 
-        private const int MAP_SIZE = 9;
-
-        private static int[,] map;
-        private static int playerX;
-        private static int playerY;
-        private const string playerCharacter = ":)";
-
-        public void Run() // Neue "Main"
+        public void Run()
+        // Ersetzt die Main Methode in Program.cs
         {
             WelcomeMessage();
-            UserInputMapSize();
+            InstructionMessage();
+            MapSize();
             DrawMap();
-            PrintMap();
-
-            // __________________________________________________________
-            // Trouple - Einfach NEIN!
-
-            // (int Left, int Top) cursorPos = Console.GetCursorPosition();
-            // Console.SetCursorPosition(playerX, playerY);
-            // Console.Write(playerCharacter);
-            // Console.SetCursorPosition(cursorPos.Left, cursorPos.Top);
-
-            // __________________________________________________________
-
-            DrawPlayer();
-
-            Console.ReadLine();
         }
 
-        public void WelcomeMessage()
+        private void WelcomeMessage()
+        // Bisschen was fürs Auge.
         {
-            Console.WriteLine(" ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄    ▄▄▄▄▄▄   ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ \r\n█       █       █       █       █       █       █  █   ▄  █ █       █       █  █▄█  █\r\n█    ▄▄▄█  ▄▄▄▄▄█       █   ▄   █    ▄  █    ▄▄▄█  █  █ █ █ █   ▄   █   ▄   █       █\r\n█   █▄▄▄█ █▄▄▄▄▄█     ▄▄█  █▄█  █   █▄█ █   █▄▄▄   █   █▄▄█▄█  █ █  █  █ █  █       █\r\n█    ▄▄▄█▄▄▄▄▄  █    █  █       █    ▄▄▄█    ▄▄▄█  █    ▄▄  █  █▄█  █  █▄█  █       █\r\n█   █▄▄▄ ▄▄▄▄▄█ █    █▄▄█   ▄   █   █   █   █▄▄▄   █   █  █ █       █       █ ██▄██ █\r\n█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄█ █▄▄█▄▄▄█   █▄▄▄▄▄▄▄█  █▄▄▄█  █▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄█   █▄█\r\n");
-            Console.WriteLine("Hello and Welcome to my Escape Room");
+            Console.WriteLine(" _____ ____   ____    _    ____  _____   ____   ___   ___  __  __ \r\n| ____/ ___| / ___|  / \\  |  _ \\| ____| |  _ \\ / _ \\ / _ \\|  \\/  |\r\n|  _| \\___ \\| |     / _ \\ | |_) |  _|   | |_) | | | | | | | |\\/| |\r\n| |___ ___) | |___ / ___ \\|  __/| |___  |  _ <| |_| | |_| | |  | |\r\n|_____|____/ \\____/_/   \\_\\_|   |_____| |_| \\_\\\\___/ \\___/|_|  |_|");
+            Console.WriteLine("__        _____ ____  _   _          \r\n\\ \\      / /_ _/ ___|| | | |         \r\n \\ \\ /\\ / / | |\\___ \\| |_| |         \r\n  \\ V  V /  | | ___) |  _  |         \r\n __\\_/\\_/__|___|____/|_|_|_|_  _   _ \r\n| ____|  _ \\_ _|_   _|_ _/ _ \\| \\ | |\r\n|  _| | | | | |  | |  | | | | |  \\| |\r\n| |___| |_| | |  | |  | | |_| | |\\  |\r\n|_____|____/___| |_| |___\\___/|_| \\_|");
             Console.ReadKey();
             Console.Clear();
         }
 
-        public void UserInputMapSize()
+        private void InstructionMessage()
+        // Viel Bla Bla...
         {
-
+            Console.WriteLine("Find the key (O╣) and open the door (▒▒)");
+            Console.WriteLine();
+            Console.WriteLine("Key assignment");
+            Console.WriteLine("Up = \u2191");
+            Console.WriteLine("Down = \u2193");
+            Console.WriteLine("Left = \u2190");
+            Console.WriteLine("Right = \u2192");
+            Console.ReadKey();
+            Console.Clear();
         }
 
-        public void DrawMap()
-            // Komplette Initialisierung der Map
+        private void MapSize()
+        // Es kommt nicht auf die Größe an... Hab ich gehört! - Hier wir der Spieler aufgefordert die Map Größe zu bestimmen, ja auch TryParse ist mit drin ^^ Doofheit der Menschen und so.
+        {
+            do
+            {
+                Console.WriteLine("Please enter a number for the room size. (number between 5-15):");
+
+                if (int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    if (userInput >= 5 && userInput <= 15)
+                    {
+                        MAP_SIZE = userInput;
+                        isValidInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Oops, that wasn't right... Try again :(");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            } while (!isValidInput);
+
+            Console.Clear();
+        }
+
+        private void DrawMap()
+        // Phantasie ist schön aber es zu sehen mach mehr Spaß! Hier wird die Map in die Konsole geprintet (warum heißt die Methode dann eigentlich "Draw" und nicht "Print" o.O), ach ja die Spielerwegung beginnt auch hier. 
+        {
+            InitializeMap();
+            DrawKey();
+            DrawDoor();
+
+            while (isRunning)
+            {
+                Console.Clear();
+                PrintMapAndCharacter();
+
+                ConsoleKeyInfo keyInput = Console.ReadKey();
+
+                HandleUserInput(keyInput);
+                TryPassThroughWall(); // Hier aufrufen, nachdem der Spieler versucht hat, sich zu bewegen.
+                CheckCharacterPosition();
+            }
+        }
+
+        private void InitializeMap()
+        // Hier bekommt die Map langsam Hand und Fuß ähhh Wand und Boden....
         {
             map = new int[MAP_SIZE, MAP_SIZE];
             for (int y = 0; y < MAP_SIZE; y++)
             {
                 for (int x = 0; x < MAP_SIZE; x++)
                 {
-                    if(y == 0 || x == 0 || y == MAP_SIZE -1 || x == MAP_SIZE -1)
+                    if (y == 0 || x == 0 || y == MAP_SIZE - 1 || x == MAP_SIZE - 1)
                     {
                         map[x, y] = (int)EMapTiles.wall;
                     }
                     else
                     {
-                        map[x, y] = (int)EMapTiles.free;
+                        map[x, y] = (int)EMapTiles.floor;
                     }
                 }
             }
+
             playerX = MAP_SIZE / 2;
             playerY = MAP_SIZE / 2;
-
-            bool isRunning = true;
-            while (isRunning)
-            {
-                Console.Clear();
-                PrintMap();
-                ConsoleKeyInfo keyInput = Console.ReadKey();
-                switch (keyInput.Key)
-                {
-                    case ConsoleKey.Escape:
-                        isRunning = false;
-                        break;
-                    case ConsoleKey.W:
-                        break;
-                    case ConsoleKey.A:
-                        break;
-                    case ConsoleKey.S:
-                        break;
-                    case ConsoleKey.D:
-                        break;
-                    case ConsoleKey.UpArrow:
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        break;
-                    case ConsoleKey.DownArrow:
-                        break;
-                    case ConsoleKey.RightArrow:
-                        break;
-                }
         }
 
-        private static void PrintMap()
+        private void HandleUserInput(ConsoleKeyInfo keyInput)
+        // Push the Button... Hier werden Nägel mit Köpf... äh knöpfen gemacht, oh und die Spieler Position wird Aktualisiert.
+        {
+            int prevPlayerX = playerX;
+            int prevPlayerY = playerY;
+
+            switch (keyInput.Key)
+            {
+                case ConsoleKey.Escape:
+                    isRunning = false;
+                    Console.Clear();
+                    break;
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    playerY--;
+                    break;
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    playerX--;
+                    break;
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    playerY++;
+                    break;
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    playerX++;
+                    break;
+            }
+
+            TryPassThroughWall(); // Versuche, durch die Wand zu gehen, bevor die Position überprüft wird.
+            CheckCharacterPosition(); // Überprüfe und korrigiere die Spielerposition.
+
+            HandleKeyCollection(); // Füge die Überprüfung für Schlüssel-Einsammlung hinzu.
+        }
+
+        private void CheckCharacterPosition()
+        // Player Position... Check! Hier wird geschaut das der Charackter nicht ausbüchsen kann.
+        {
+            if (playerX <= 0)
+            {
+                playerX = 1;
+            }
+            else if (playerX >= MAP_SIZE - 1)
+            {
+                playerX = MAP_SIZE - 2;
+            }
+
+            if (playerY <= 0)
+            {
+                playerY = 1;
+            }
+            else if (playerY >= MAP_SIZE - 1)
+            {
+                playerY = MAP_SIZE - 2;
+            }
+        }
+
+        private void PrintMapAndCharacter()
+        // Ahhhh hier haben wir das Printen der Map und Characters
         {
             for (int y = 0; y < MAP_SIZE; y++)
             {
-                for(int x = 0; x < MAP_SIZE; x++)
+                for (int x = 0; x < MAP_SIZE; x++)
                 {
-                    if(y == playerY && x == playerX)
+                    if (y == playerY && x == playerX)
                     {
-                        Console.Write(playerCharacter + "");
+                        Console.Write(playerChar);
                     }
                     else
                     {
                         int mapValue = map[x, y];
                         Console.Write(mapTileChar[mapValue + 1]);
                     }
+                    Console.Write("");
                 }
 
                 Console.WriteLine();
             }
         }
 
-        public void DrawPlayer()
+        private void DrawKey()
+        // Hier wird der Schlüssel zufällig auf der Map gespwant - Hier kein Witz, is ne ernste Sache! ;)
         {
+            System.Random rnd = new Random();
 
+            int keyX;
+            int keyY;
+
+            do
+            {
+                keyX = rnd.Next(1, MAP_SIZE - 1);
+                keyY = rnd.Next(1, MAP_SIZE - 1);
+            } while (map[keyX, keyY] != (int)EMapTiles.floor);
+
+            map[keyX, keyY] = (int)EMapTiles.key; // Markiere die Position des Schlüssels auf der Karte.
         }
+
+        private void HandleKeyCollection()
+        {
+            if (map[playerX, playerY] == (int)EMapTiles.key)
+            {
+                isKeyCollected = true;
+                map[playerX, playerY] = (int)EMapTiles.floor; // Lösche den Schlüssel von der Karte.
+
+                OpenDoor(); // Neu hinzugefügte Methode zum Öffnen der Tür.
+            }
+        }
+
+        private void DrawDoor()
+        {
+            System.Random rndDoor = new Random();
+
+            int doorX;
+            int doorY;
+
+            do
+            {
+                doorX = rndDoor.Next(0, MAP_SIZE - 2);
+                doorY = rndDoor.Next(0, MAP_SIZE - 2);
+            } while (map[doorX, doorY] != (int)EMapTiles.wall);
+
+            map[doorX, doorY] = (int)EMapTiles.closedDoor;
+        }
+
+        private void OpenDoor()
+        {
+            for (int x = 0; x < MAP_SIZE; x++)
+            {
+                for (int y = 0; y < MAP_SIZE; y++)
+                {
+                    if (map[x, y] == (int)EMapTiles.closedDoor)
+                    {
+                        map[x, y] = (int)EMapTiles.openDoor;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void CheckExitGame()
+        {
+            if (map[playerX, playerY] == (int)EMapTiles.openDoor && isKeyCollected)
+            {
+                Console.WriteLine("Glückwunsch! Du hast das Spiel gewonnen!");
+                isRunning = false;
+                return;
+            }
+
+            if (isKeyCollected && map[playerX, playerY] == (int)EMapTiles.closedDoor)
+            {
+                Console.WriteLine("Du hast die Tür geöffnet, betrete sie, um das Spiel zu beenden!");
+                // Überprüfe, ob der Weg zur Tür durch eine Wand blockiert ist, und umgehe die Wand.
+                TryPassThroughWall();
+            }
+        }
+
+        private void TryPassThroughWall()
+        {
+            int potentialX = playerX;
+            int potentialY = playerY;
+
+            // Überprüfe, ob der Spieler auf eine Wand zugeht und versuche, die Wand zu umgehen.
+            if (playerX < MAP_SIZE - 2 && map[playerX + 1, playerY] == (int)EMapTiles.wall)
+            {
+                potentialX = playerX + 1;
+            }
+            else if (playerX > 1 && map[playerX - 1, playerY] == (int)EMapTiles.wall)
+            {
+                potentialX = playerX - 1;
+            }
+
+            if (playerY < MAP_SIZE - 2 && map[playerX, playerY + 1] == (int)EMapTiles.wall)
+            {
+                potentialY = playerY + 1;
+            }
+            else if (playerY > 1 && map[playerX, playerY - 1] == (int)EMapTiles.wall)
+            {
+                potentialY = playerY - 1;
+            }
+
+            // Überprüfe, ob die potenzielle Position durch eine Wand blockiert ist.
+            if (map[potentialX, potentialY] != (int)EMapTiles.wall)
+            {
+                playerX = potentialX;
+                playerY = potentialY;
+            }
+        }
+
     }
 }
